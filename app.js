1dbe1f7ac2d8d4a5273604b68aede4f598262705
robertinit@comcast.net <robertinit@comcast.net>
2020-04-07 19:09:15 -0600

    add form and public route

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 80;
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/public", express.static("public"));
+app.set("view engine", "ejs");
+app.post("/test", (req, res) => {
+  res.send(req.body);
+});
 app.use((req, res) => {
-  res.send("test");
+  res.render("index.ejs");
 });
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
