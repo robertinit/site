2a0ec7ea757651e35eae65a33d0759e48ae28748
Robert Mangum <rmangum@grangermedical.com>
2020-04-09 11:08:34 -0600

    update form add error handling

--- app.js
+++ app.js
@@ -7,56 +7,65 @@ const {
   getFormName,
   // getFormList,
   saveResponse,
-  getFormID
-} = require('./soap')
+  getFormID,
+} = require("./soap");
 const PORT = process.env.PORT;
-const APIKey = process.env.APIKEY
+const APIKey = process.env.APIKEY;
 //middleware
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
 app.use("/public", express.static("public"));
 app.set("view engine", "ejs");
 
 //routes
 app.post("/submit", (req, res) => {
-  const {
-    id,
-    formID
-  } = req.body
-  delete req.body.id
-  delete req.body.formID
-  data = JSON.stringify(req.body)
-  saveResponse(APIKey, formID, id, `${data}`).then(e => {
-    if (e == 'true') {
-      res.render('thanks.ejs')
-    } else {
-      res.render('invalid.ejs')
-    }
-  }).catch(err => {
-    res.render('something went wrong.ejs')
-    console.log(err)
-  })
+  const { id, formID } = req.body;
+  delete req.body.id;
+  delete req.body.formID;
+  data = JSON.stringify(req.body);
+  saveResponse(APIKey, formID, id, `${data}`)
+    .then((e) => {
+      if (e == "true") {
+        res.render("thanks.ejs");
+      } else {
+        res.render("invalid.ejs");
+      }
+    })
+    .catch((err) => {
+      res.render("something went wrong.ejs");
+      console.log(err);
+    });
 });
 app.get("/id::id", (req, res) => {
-  const {
-    id
-  } = req.params
-  isActive(APIKey, id).then(answer => {
-    if (answer == 'true') {
-      getFormID(APIKey, id).then(form => {
-        getFormName(APIKey, form).then(ejs => {
-          res.render(`${ejs}.ejs`, {
-            id
-          })
-        })
-      })
-    } else {
-      res.render('404.ejs')
-    }
-  })
-})
+  const { id } = req.params;
+  isActive(APIKey, id)
+    .then((answer) => {
+      if (answer == "true") {
+        getFormID(APIKey, id).then((form) => {
+          getFormName(APIKey, form)
+            .then((ejs) => {
+              res.render(`${ejs}.ejs`, {
+                id,
+              });
+            })
+            .catch((err) => {
+              res.render("something went wrong.ejs");
+              console.log(err);
+            });
+        });
+      } else {
+        res.render("invalid.ejs");
+      }
+    })
+    .catch((err) => {
+      res.render("something went wrong.ejs");
+      console.log(err);
+    });
+});
 app.use((req, res) => {
   res.status(404).render("404.ejs");
 });
