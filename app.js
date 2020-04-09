7f9e8ea0014e1f394ebb885f929b24e39b42ed25
robertinit <robertinit@comcast.net>
2020-04-09 10:24:40 -0600

    added soap and additional views

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = 80;
-
+const {
+  // createToken,
+  isActive,
+  getFormName,
+  // getFormList,
+  saveResponse,
+  getFormID
+} = require('./soap')
+const PORT = process.env.PORT;
+const APIKey = process.env.APIKEY
 //middleware
 app.use(bodyParser.urlencoded({
   extended: false
@@ -12,55 +20,48 @@ app.use("/public", express.static("public"));
 app.set("view engine", "ejs");
 
 //routes
-app.post("/test", (req, res) => {
-  console.log(req.body)
+app.post("/submit", (req, res) => {
   const {
-    name,
-    address,
-    idea,
-    freeText,
-    clickTime,
-    id
+    id,
+    formID
   } = req.body
-  // res.send(req.body);
-  if (checkSubmitted(id)) {
-    res.render('submitted.ejs', {
-      name,
-      address,
-      idea,
-      freeText,
-      clickTime,
-      submitted: Date(),
-      id
-    })
-  } else {
-    res.send('form already submitted')
-  }
-});
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+  delete req.body.id
+  delete req.body.formID
+  data = JSON.stringify(req.body)
+  console.log(data)
+  saveResponse(APIKey, formID, id, `${data}`).then(e => {
+    console.log(e)
+    if (e == 'true') {
+      res.render('thanks.ejs')
+    } else {
+      res.render('invalid.ejs')
+    }
+  }).catch(err => {
+    res.render('something went wrong.ejs')
+    console.log(err)
+  })
 });
 app.get("/id::id", (req, res) => {
   const {
     id
   } = req.params
-  //query with ID 
-  //if not clicked:
-  if (checkID(id)) {
-    const form = requestForm(id)
-    res.render(form, {
-      id
-    })
-  } else {
-    res.render('404.ejs')
-  }
+  isActive(APIKey, id).then(answer => {
+    if (answer == 'true') {
+      getFormID(APIKey, id).then(form => {
+        getFormName(APIKey, form).then(ejs => {
+          res.render(`${ejs}.ejs`, {
+            id
+          })
+        })
+      })
+    } else {
+      console.log(answer)
+      res.render('404.ejs')
+    }
+  })
 })
 app.use((req, res) => {
   res.status(404).render("404.ejs");
 });
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
-
-const checkID = (id) => true //check if id active
-const requestForm = (id) => 'index.ejs' //get form for id
-const checkSubmitted = (id) => false //is it submitted
